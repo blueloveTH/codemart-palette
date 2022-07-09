365a019e146210117868926092398e0621c058e7
blueloveTH <blueloveth@dreamers.guru>
2022-07-09 18:56:42 +0800

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -25,10 +25,10 @@ def solve_eq():
         return (res ** 2).sum()
 
     r = None
-    for _ in range(4):
+    for _ in range(1):
         p0 = np.random.uniform(0, 1, len(cmyk_cpnts))
-        # new_r = dual_annealing(loss, bounds=[(0,1)]*len(p0))
-        new_r = least_squares(loss, x0=p0, bounds=(0, 1))
+        new_r = dual_annealing(loss, bounds=[(0,1)]*len(p0), maxiter=800)
+        # new_r = least_squares(loss, x0=p0, bounds=(0, 1))
         #new_r = minimize(loss, p0, bounds=Bounds(0, 1))
         if r is None:
             r = new_r
